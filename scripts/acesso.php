b3556bd0199579ddc735003bb28e6034ef40d941
Estevão P. Rolim <estevao.prolim@gmail.com>
2018-05-05 16:36:06 -0300

    Add files via upload

---
+++ scripts/acesso.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway|Source+Sans+Pro" rel="stylesheet">
+	<meta charset="UTF-8">
+	<title>Acesso grêmio</title>
+</head>
+
+<body>
+	<p>
+		<?php
+    include "connect.php";
+    $ra = $_POST['ra'];
+    $senha = $_POST['senha'];
+        
+    $sql = "SELECT * FROM portal.users WHERE ra = $ra";
+    $res = pg_query($conecta, $sql);
+    if ($res == NULL)
+    {
+        ECHO "RA inexistente!";
+        exit;
+    }
+    $vals = pg_fetch_object($res);
+        //ECHO $vals->senha."/fdp/".$senha;
+    if ($vals->senha != $senha)
+    {
+        ECHO "Senha incorreta!";
+        exit;
+    }
+    if ($vals->adm != 's' || $vals->excluido != 'n' || $vals->ban != 'n')
+    {
+        ECHO "Você não possui permissão para acessar esse site!";
+        exit;
+    }
+    /*else
+    {*/
+        $sql = "SELECT * FROM portal.mensagens";
+        $res = pg_query($conecta, $sql);
+        $cont = pg_num_rows($res);
+        while($cont >= 1)
+        {
+            $rsql = "SELECT * FROM portal.mensagens WHERE id = $cont";
+            $valores = pg_query($conecta, $rsql);
+            $r = pg_fetch_object($valores);
+            ECHO "Nome: ".$r->nome."<br>Curso: ".$r->curso."<br>Mensagem: ".$r->mensagem."<br>Data: ".$r->data."<br><hr>";
+            $cont--;
+        }
+    //}
+    pg_close($conecta);
+    ?>
+	</p>
+</body>
+
+</html>
---
+++ scripts/sessao.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway|Source+Sans+Pro" rel="stylesheet">
+	<meta charset="UTF-8">
+	<title>Realizando login...</title>
+</head>
+<script type="text/javascript">
+	window.onload = function espera() {
+		setTimeout(volta, 1);
+	}
+
+	function volta() {
+		window.history.go(-1);
+	}
+
+</script>
+
+<body>
+	<?php
+        include "connect.php";
+        $ra = $_POST['ra'];
+        $senha = $_POST['senha'];
+        $sql = "SELECT * FROM portal.users WHERE ra = $ra";
+        $res = pg_query($conecta, $sql);
+        if ($res == NULL)
+        {
+            ECHO "RA inexistente!";
+            exit;
+        }
+        $vals = pg_fetch_object($res);
+            //ECHO $vals->senha."/fdp/".$senha;
+        if ($vals->senha != $senha)
+        {
+            ECHO "Senha incorreta!";
+            exit;
+        }
+        if ($vals->excluido != 'n' || $vals->ban != 'n')
+        {
+            ECHO "Conta indisponível!";
+            exit;
+        }
+        session_start();
+        $_SESSION['ra'] = $ra;
+        $_SESSION['senha'] = $senha;
+        pg_close($conecta); 
+    ?>
+</body>
+
+</html>
